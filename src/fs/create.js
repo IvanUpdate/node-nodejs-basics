432bc2ec0eb9f0dc16b13197d669b32ec49bb959
IvanUpdate <mironovvan99@gmail.com>
2023-06-19 18:21:27 +0300

    add fs fuctionality

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
-const create = async () => {
-    // Write your code here 
+import {stat, writeFile} from "fs";
+import {join, dirname} from "path";
+import {fileURLToPath} from 'url';
+
+export const create = async () => {
+    const directory = dirname(fileURLToPath(import.meta.url));
+    const filePath = join(directory, 'files', 'fresh.txt');
+    const text = 'I am fresh and young';
+
+    await stat(filePath, (err, stat) => {
+        if (stat) {
+            throw new Error("FS operation failed");
+        } else {
+            writeFile(filePath, text, (error) => {
+                if (error) throw err;
+            })
+        }
+    })
 };
 
-await create();
+create();
